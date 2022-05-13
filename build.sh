fbe8c84249d7d53905c5c39139665d659c2d8ece
cpasjuste <cpasjuste@gmail.com>
2022-05-13 15:11:40 +0200

    retroroot: wip

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+docker run --privileged --rm -v /dev:/dev:ro -v "$PWD":/runner -w /runner \
+  -e ALPINE_BRANCH=latest-stable \
+  -e ALPINE_MIRROR="http://dl-cdn.alpinelinux.org/alpine" \
+  -e ARCH=aarch64 \
+  runner ./make-image
+
